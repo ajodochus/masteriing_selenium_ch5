9ff5b80ec885b4f6b7d86b661a681c050e68061d
ajodochus <ajodochus@googlemail.com>
2017-12-11 08:35:21 +0100

    - add log functionallity - set logs to gitignore

--- src/test/java/com/masteringselenium/fluent_page_objects/LoginPage.java
+++ src/test/java/com/masteringselenium/fluent_page_objects/LoginPage.java
@@ -1,11 +1,13 @@
 package com.masteringselenium.fluent_page_objects;
 
 import com.masteringselenium.DriverFactory;
+
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
 import org.openqa.selenium.support.PageFactory;
 
+import org.apache.log4j.Logger;
 public class LoginPage {
 
     @FindBy(how = How.ID, using = "modlgn-username")
@@ -16,12 +18,16 @@ public class LoginPage {
 
     @FindBy(how = How.XPATH, using = ".//*[@id='form-login-submit']/div/button")
     private  WebElement loginButtonLocator;
+    
+    private static Logger logger = Logger.getLogger(LoginPage.class);
+    
 
     public LoginPage() throws Exception {
         PageFactory.initElements(DriverFactory.getDriver(), this);
     }
 
     public LoginPage enterUsername(String username, String password) {
+    	logger.info("login with username and password");
         usernameLocator.sendKeys(username);
         passwordLocator.sendKeys(password);
         loginButtonLocator.click();
